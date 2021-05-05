a2e9566ccad4750e95a38e9626a41f4d69086ba0
matibraun <matibraun@gmail.com>
2021-05-05 12:32:20 +0200

    funcion para truco andando

--- main.ts
+++ main.ts
@@ -140,6 +140,76 @@ function calculatePointsEnvido(envidoPlays, points) {
     }
 }
 
+function checkPositionCard(card){
+    const sampleDeck = [ 
+        [[ 1, 'espada' ]],
+        [[ 1, 'basto' ]],
+        [[ 7, 'espada' ]],
+        [[ 7, 'oro' ]],
+        [[ 3, 'espada' ],
+        [ 3, 'basto' ],
+        [ 3, 'oro' ],
+        [ 3, 'copa' ]],
+        [[ 2, 'espada' ],
+        [ 2, 'basto' ],
+        [ 2, 'oro' ],
+        [ 2, 'copa' ]],
+        [[ 1, 'oro' ],
+        [ 1, 'copa' ]],
+        [[ 12, 'espada' ],
+        [ 12, 'basto' ],
+        [ 12, 'oro' ],
+        [ 12, 'copa' ]],
+        [[ 11, 'espada' ],
+        [ 11, 'basto' ],
+        [ 11, 'oro' ],
+        [ 11, 'copa' ]],
+        [[ 10, 'espada' ],
+        [ 10, 'basto' ],
+        [ 10, 'oro' ],
+        [ 10, 'copa' ]],
+        [[ 7, 'basto' ],
+        [ 7, 'copa' ]],
+        [[ 6, 'espada' ],
+        [ 6, 'basto' ],
+        [ 6, 'oro' ],
+        [ 6, 'copa' ]],
+        [[ 5, 'espada' ],
+        [ 5, 'basto' ],
+        [ 5, 'oro' ],
+        [ 5, 'copa' ]],
+        [[ 4, 'espada' ],
+        [ 4, 'basto' ],
+        [ 4, 'oro' ],
+        [ 4, 'copa' ]],
+    ]
+    
+    let i
+    let j
+    
+    for (i = 0; i < sampleDeck.length; i++) {
+        for (j = 0; j < sampleDeck[i].length; j++) {
+            if (sampleDeck[i][j].includes(card[0]) && sampleDeck[i][j].includes(card[1])) {
+                return i
+            }
+        }
+    }
+}
+
+function checkWhichCardWins(card1, card2) {
+    const positionCard1 = checkPositionCard(card1)
+    const positionCard2 = checkPositionCard(card2)
+    if (positionCard1 === positionCard2) {
+        return 'tie'
+    }
+    if (positionCard1 < positionCard2) {
+        return 0
+    }
+    if (positionCard1 > positionCard2) {
+        return 1
+    }
+
+}
 
 
 
@@ -228,6 +298,7 @@ function reducer(state, action): State {
                     'Envido',
                     'Real Envido',
                     'Falta Envido',
+                    'Truco',
                     'Jugar Carta 1',
                     'Jugar Carta 2',
                     'Jugar Carta 3',
